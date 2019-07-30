add3c7eab87d03a70a19707dbe013ea2aff13b63
shinta <flekystyley@ezweb.ne.jp>
2019-07-31 00:00:54 +0900

    remove no used code;

--- main.cpp
+++ main.cpp
@@ -12,59 +12,6 @@ static const std::string url = "tcp://127.0.0.1:3306";
 static const std::string user = "shinta";
 static const std::string password = "djgm257890hujm6nvr54hwe";
 
-// void connnect_mysql()
-// {
-//     sql::Driver *driver;
-//     sql::Connection *conn;
-//     sql::Statement *stmt;
-//     sql::ResultSet *rs;
-//     sql::PreparedStatement *prep_stmt;
-    
-//     try
-//     {
-//         driver = get_driver_instance();
-
-//         conn = driver->connect(url, user, password);
-
-//         conn->setSchema("provinces");
-
-//         // Statement example
-//         stmt = conn->createStatement();
-
-//         rs = stmt->executeQuery("SELECT * FROM provinces");
-
-//         std::cout << "Retreived row count: " << rs->rowsCount() << std::endl;
-
-//         for (size_t i = 0; i < rs->rowsCount(); ++i) {
-//             rs->next();
-//             std::cout << "Next string: " <<  rs->getString("province") <<  std::endl;
-//         }
-
-//         // Prepared statement example
-//         prep_stmt = conn->prepareStatement("INSERT INTO provinces (province, population) VALUES (?, ?)");
-
-//         prep_stmt->setString(1, "test province");
-//         prep_stmt->setString(2, "3");
-
-//         int update_count = prep_stmt->executeUpdate();
-
-//         std::cout << "Updated row count: " << update_count << std::endl;
-
-//         delete rs;
-//         delete stmt;
-//         delete prep_stmt;
-//         conn->close();
-//         delete conn;
-//     }
-//     catch (sql::SQLException &e)
-//     {
-//         std::cout << "Error Description:" << e.what() << std::endl;
-//         std::cout << "Error Code: " << e.getSQLState() << std::endl;
-//     }
-
-//     return;
-// }
-
 void configuration_handler(const utility::string_t& address)
 {
     uri_builder uri(address);
