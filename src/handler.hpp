fa7da4960c923d0b83a35dc930218910eb7f7f05
shinta <flekystyley@ezweb.ne.jp>
2019-08-16 00:11:05 +0900

    fix handle.cpp;

--- src/handler.hpp
+++ src/handler.hpp
@@ -5,21 +5,10 @@
 
 #include "messagetypes.hpp"
 
-#include "cpprest/json.h"
-#include "cpprest/http_listener.h"
-#include "cpprest/uri.h"
-#include "cpprest/asyncrt_utils.h"
-
-using namespace std;
-using namespace web;
-using namespace utility;
-using namespace http;
-using namespace web::http::experimental::listener;;
-
 class Handler
 {
 public:
-	Handler() {}
+	Handler();
 	Handler(utility::string_t url);
 	~Handler();
 
